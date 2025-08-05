e7c31e782977432c0319bf83f4473832c3f9804f
farazAhmed03 <cs120212072.kust.edu.pk@gmail.com>
2025-08-05 14:11:30 +0500

    Update Socket.jsx

--- src/SocketService/Socket.jsx
+++ src/SocketService/Socket.jsx
@@ -3,6 +3,11 @@ import { io } from "socket.io-client";
 
 const socket = io("https://faithful-motivation-production.up.railway.app", {
   withCredentials: true,
+  transports: ['websocket', 'polling'],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 2000,
 });
+
 
 export default socket;
